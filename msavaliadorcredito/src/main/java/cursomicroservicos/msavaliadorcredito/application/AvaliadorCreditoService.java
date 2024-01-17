4d3adbedcccd71982fc64ac1b661c8ea0ddc9831
Robson <robson.080490@gmail.com>
2024-01-17 09:06:44 -0300

    adicionando busca dos dados de cartoes pelo microservice de cartoes.

--- msavaliadorcredito/src/main/java/cursomicroservicos/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/cursomicroservicos/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,22 +1,29 @@
 package cursomicroservicos.msavaliadorcredito.application;
 
+import cursomicroservicos.msavaliadorcredito.domain.model.CartaoCliente;
 import cursomicroservicos.msavaliadorcredito.domain.model.DadosCliente;
 import cursomicroservicos.msavaliadorcredito.domain.model.SituacaoCliente;
+import cursomicroservicos.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import cursomicroservicos.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
     private final ClienteResourceClient clientsClient;
+    private final CartoesResourceClient cartoesClient;
     public SituacaoCliente obterSituacaoCliente(String cpf) {
         ResponseEntity<DadosCliente> dadosClienteResponse = clientsClient.dadosCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
 
         return SituacaoCliente
                 .builder()
                 .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesResponse.getBody())
                 .build();
     }
 }
