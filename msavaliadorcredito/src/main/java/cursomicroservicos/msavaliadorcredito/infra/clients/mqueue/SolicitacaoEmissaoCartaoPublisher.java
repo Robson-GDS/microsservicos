d0002bdf6c523e1e7ff70bd4184d42ce8407a6db
Robson <robson.080490@gmail.com>
2024-01-23 08:38:03 -0300

    Criando o Publisher para a fila de Emissão de cartões.

---
+++ msavaliadorcredito/src/main/java/cursomicroservicos/msavaliadorcredito/infra/clients/mqueue/SolicitacaoEmissaoCartaoPublisher.java
@@ -0,0 +1,27 @@
+package cursomicroservicos.msavaliadorcredito.infra.clients.mqueue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cursomicroservicos.msavaliadorcredito.domain.model.DadosSolicitacaoEmissaoCartao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SolicitacaoEmissaoCartaoPublisher {
+    private final RabbitTemplate rabbitTemplate;
+    private final Queue queueEmissaoCartoes;
+
+    public void solicitarCartao(DadosSolicitacaoEmissaoCartao dadosSolicitacaoEmissaoCartao) throws JsonProcessingException {
+        var json = convertIntoJson(dadosSolicitacaoEmissaoCartao);
+        rabbitTemplate.convertAndSend(queueEmissaoCartoes.getName(), json);
+    }
+
+    private String convertIntoJson(DadosSolicitacaoEmissaoCartao dadosSolicitacaoEmissaoCartao) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        var json = mapper.writeValueAsString(dadosSolicitacaoEmissaoCartao);
+        return json;
+    }
+}
